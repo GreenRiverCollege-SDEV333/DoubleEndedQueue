01fcb950100a80f4fb1345069fee361b3189dc66
Stewart Lovell <lovell.stewart@student.greenriver.edu>
2023-02-08 14:30:48 -0800

    finished implementation

--- src/ResizingArrayDeque.java
+++ src/ResizingArrayDeque.java
@@ -32,7 +32,21 @@ public class ResizingArrayDeque<ItemType> implements Deque<ItemType> {
     @Override
     public void addFirst(ItemType item) {
         // consider the case of adding to an empty list
-        // consider the case of adding to a non-empty list
+        if (size == 0) {
+            data[0] = item;
+            size++;
+        }else { // consider the case of adding to a non empty list
+            // check if the array needs to be resized
+            checkSize();
+            // loop from the back of the array to the front
+            for(int i=size-1; i>=0; i--) {
+                // set the next index to the current index
+                data[i+1] = data[i];
+            }
+            // set the first index to the item
+            data[0] = item;
+            size++;
+        }
 
         // There is a private helper method checkSize() defined below to check/resize
         // that you can call as needed to check if the array is full and resize it.
@@ -46,7 +60,16 @@ public class ResizingArrayDeque<ItemType> implements Deque<ItemType> {
     @Override
     public void addLast(ItemType item) {
         // consider the case of adding to an empty list
-        // consider the case of adding to a non-empty list
+        if (size == 0) {
+            data[0] = item;
+            size++;
+        } else { // consider the case of adding to a non-empty list
+            // check for a resize
+            checkSize();
+            // set the last (empty) index to the item
+            data[size] = item;
+            size++;
+        }
 
         // There is a private helper method checkSize() defined below to check/resize
         // that you can call as needed to check if the array is full and resize it.
@@ -60,17 +83,27 @@ public class ResizingArrayDeque<ItemType> implements Deque<ItemType> {
     @Override
     public ItemType removeFirst() {
         // check if empty
-        // if empty: do nothing and return null
-
-        // if there's only one item: is this a special case?
-
-        // if not empty:
-        // 0. figure out a way to access the item in the front
-        // 1. make a variable to save a copy of the item at the front
-        // 2. remove the item at the front
-        // 3. return the variable that has the saved copy of the item at the front
-
-        return null;
+        if (size == 0) {
+            // if empty: do nothing and return null
+            return null;
+        } else if (size == 1) {// if there's only one item: is this a special case?
+            ItemType removed = data[0];
+            data[0] = null;
+            size--;
+            return removed;
+        } else {// if not empty:
+            // 0. figure out a way to access the item in the front
+            ItemType removed = data[0];
+            // 1. make a variable to save a copy of the item at the front
+            // 2. remove the item at the front
+            for (int i=0; i<size; i++) {
+                //shift all items in the array back one index
+                data[i] = data[i+1];
+            }
+            size--;
+            // 3. return the variable that has the saved copy of the item at the front
+            return removed;
+        }
     }
 
     /**
@@ -82,16 +115,23 @@ public class ResizingArrayDeque<ItemType> implements Deque<ItemType> {
     public ItemType removeLast() {
         // check if empty
         // if empty: do nothing and return null
-
-        // if there is only one item: is this a special case?
-
-        // if not empty, has more than one item:
-        // 0. figure out a way to access the item in the back
-        // 1. make a variable to save a copy of the item at the back
-        // 2. remove the item at the back
-        // 3. return the variable that has the saved copy of the item at the back
-
-        return null;
+        if (size == 0) {
+            return null;
+        } else if (size == 1) {// if there's only one item: is this a special case?
+            ItemType removed = data[0];
+            data[0] = null;
+            size--;
+            return removed;
+        } else { // if not empty, has more than one item:
+            // 0. figure out a way to access the item in the back
+            // 1. make a variable to save a copy of the item at the back
+            ItemType removed = data[size-1];
+            // 2. remove the item at the back
+            data[size-1] = null;
+            size--;
+            // 3. return the variable that has the saved copy of the item at the back
+            return removed;
+        }
     }
 
     // helper method to check to see if the size has reached the capacity
@@ -115,5 +155,13 @@ public class ResizingArrayDeque<ItemType> implements Deque<ItemType> {
             // Optional:
             temp = null;
         } // end of if (need to resize)
+    }
+
+    public String toString() {
+        String result = "";
+        for (int i=0; i<size; i++) {
+            result += data[i] + " ";
+        }
+        return result;
     }
 }
--- src/SinglyLinkedDeque.java
+++ src/SinglyLinkedDeque.java
@@ -35,7 +35,23 @@ public class SinglyLinkedDeque<ItemType> implements Deque<ItemType> {
     @Override
     public void addFirst(ItemType item) {
         // consider the case of adding to an empty list
-        // consider the case of adding to a non-empty list
+        if (size == 0) {
+            Node newNode = new Node();
+            newNode.data = item;
+            newNode.next = null;
+            head = newNode;
+            size++;
+        } else {// consider the case of adding to a non-empty list
+            // create a new node
+            Node newNode = new Node();
+            // give the newNode the item as its data
+            newNode.data = item;
+            // set newNode's next equal to the head
+            newNode.next = head;
+            // set the head equal to the newNode
+            head = newNode;
+            size++;
+        }
     }
 
     /**
@@ -46,7 +62,28 @@ public class SinglyLinkedDeque<ItemType> implements Deque<ItemType> {
     @Override
     public void addLast(ItemType item) {
         // consider the case of adding to an empty list
-        // consider the case of adding to a non-empty list
+        if (size == 0) {
+            Node newNode = new Node();
+            newNode.data = item;
+            newNode.next = null;
+            head = newNode;
+            size++;
+        } else {// consider the case of adding to a non-empty list
+            // create a new node with the item as its data
+            Node newNode = new Node();
+            newNode.data = item;
+            newNode.next = null;
+            // loop until the end of the list, keeping track of the current node
+            Node current = head;
+            while (current.next != null) {
+                // move current to the next node
+                current = current.next;
+            }
+            // at the end of the list....
+            // set the next node equal to the new node
+            current.next = newNode;
+            size++;
+        }
     }
 
     /**
@@ -58,16 +95,22 @@ public class SinglyLinkedDeque<ItemType> implements Deque<ItemType> {
     public ItemType removeFirst() {
         // check if empty
         // if empty: do nothing and return null
-
-        // if there's only one item: is this a special case?
-
-        // if not empty:
-        // 0. figure out a way to access the item in the front
-        // 1. make a variable to save a copy of the item at the front
-        // 2. remove the item at the front
-        // 3. return the variable that has the saved copy of the item at the front
-
-        return null;
+        if (size == 0) {
+            return null;
+        } else if (size == 1) {// if there's only one item: is this a special case?
+            ItemType removed = head.data;
+            head = null;
+            size--;
+            return removed;
+        } else { // if not empty:
+            // 1. make a variable to save a copy of the item at the front
+            ItemType removed = head.data;
+            // 2. remove the item at the front
+            head = head.next;
+            // 3. return the variable that has the saved copy of the item at the front
+            size--;
+            return removed;
+        }
     }
 
     /**
@@ -79,15 +122,40 @@ public class SinglyLinkedDeque<ItemType> implements Deque<ItemType> {
     public ItemType removeLast() {
         // check if empty
         // if empty: do nothing and return null
+        if (size == 0) {
+            return null;
+        } else if (size == 1) {// if there's only one item: is this a special case?
+            ItemType removed = head.data;
+            head = null;
+            size--;
+            return removed;
+        } else { // if not empty:
+            // 0. figure out a way to access the item in the back
+            // create nodes to keep track of current and previous
+            Node previous = null;
+            Node current = head;
+            // loop to the end of the list, keeping track of current
+            while (current.next != null) {
+                previous = current;
+                current = current.next;
+            }
+            // 1. make a variable to save a copy of the item at the back
+            ItemType removed = current.data;
+            // 2. remove the item at the back
+            previous.next = null;
+            size--;
+            // 3. return the variable that has the saved copy of the item at the back
+            return removed;
+        }
+    }
 
-        // if there is only one item: is this a special case?
-
-        // if not empty, has more than one item:
-        // 0. figure out a way to access the item in the back
-        // 1. make a variable to save a copy of the item at the back
-        // 2. remove the item at the back
-        // 3. return the variable that has the saved copy of the item at the back
-
-        return null;
+    public String toString() {
+        String result = "";
+        Node current = head;
+        while (current != null) {
+            result += current.data + " ";
+            current = current.next;
+        }
+        return result;
     }
 }
