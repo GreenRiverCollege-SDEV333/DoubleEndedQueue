137b9b49d488100575c98ef50cf4be3660f1ec48
junnynoriega-arenas <junnynoriega@gmail.com>
2023-02-08 14:46:03 -0800

    my files

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,14 @@ public class Main {
 
         Deque<String> d1 = new ResizingArrayDeque<>();
         // some test code here
+        d1.addFirst("pink");
+        d1.addFirst("brown");
+        System.out.println(d1.size());
 
         Deque<String> d2 = new SinglyLinkedDeque<>();
         // some test code here
-
+        d2.addFirst("pink");
+        d2.addFirst("brown");
+        System.out.println(d2.size());
     }
 }
