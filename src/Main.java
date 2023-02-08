a5fbb6cb9529e7d1af820c0dd77754b9180a7892
TomL1100 <tom.loudon.5@gmail.com>
2023-02-08 14:42:40 -0800

    completed exam problem

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,49 @@ public class Main {
         System.out.println("Hello world!");
 
         Deque<String> d1 = new ResizingArrayDeque<>();
-        // some test code here
+        d1.addFirst("Hello");
+        d1.addFirst("Bye");
+        System.out.println(d1.size());
+        System.out.println(d1.removeFirst());
+        System.out.println(d1.size());
+        System.out.println(d1.removeFirst());
+        System.out.println(d1.size());
+        d1.addFirst("Hello");
+        d1.addFirst("Bye");
+        d1.addLast("Hola");
+        d1.addLast("Adios");
+        System.out.println(d1.size());
+        System.out.println(d1.removeLast());
+        System.out.println(d1.size());
+        System.out.println(d1.removeLast());
+        System.out.println(d1.size());
+        System.out.println(d1.removeLast());
+        System.out.println(d1.size());
+        System.out.println(d1.removeLast());
+        System.out.println(d1.size());
+
 
         Deque<String> d2 = new SinglyLinkedDeque<>();
-        // some test code here
+        d2.addFirst("Hello");
+        d2.addFirst("Bye");
+        System.out.println(d2.size());
+        System.out.println(d2.removeFirst());
+        System.out.println(d2.size());
+        System.out.println(d2.removeFirst());
+        System.out.println(d2.size());
+        d2.addFirst("Hello");
+        d2.addFirst("Bye");
+        d2.addLast("Hola");
+        d2.addLast("Adios");
+        System.out.println(d2.size());
+        System.out.println(d2.removeLast());
+        System.out.println(d2.size());
+        System.out.println(d2.removeLast());
+        System.out.println(d2.size());
+        System.out.println(d2.removeLast());
+        System.out.println(d2.size());
+        System.out.println(d2.removeLast());
+        System.out.println(d2.size());
 
     }
 }
